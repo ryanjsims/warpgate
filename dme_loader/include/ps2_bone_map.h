e18961b9fb84a9a227a201d592ebdc1b4b344807
Ryan Sims <ryanjsims@email.arizona.edu>
2022-12-04 11:40:16 -0700

    Add bone map

---
+++ dme_loader/include/ps2_bone_map.h
@@ -0,0 +1,6 @@
+#include <unordered_map>
+
+namespace utils {
+    extern std::unordered_map<uint32_t, std::string> bone_hashmap;
+    extern std::unordered_map<uint32_t, uint32_t> bone_hierarchy;
+}; //namespace utils
